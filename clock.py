1ef81b2f61f74c30c27f0cc801a001b5f85aaa3d
philskillz-coder <76884393+philskillz-coder@users.noreply.github.com>
2021-01-09 13:29:55 +0100

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,81 @@
+import turtle
+import time
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+wn.title("A Clock Made by @Philskillz")
+wn.tracer(0)
+
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.speed(0)
+pen.pensize(3)
+
+def draw_clock(h, m, s, pen):
+    pen.up()
+    pen.goto(0, 210)
+    pen.setheading(180)
+    pen.color("green")
+    pen.pendown()
+    pen.circle(210)
+
+    pen.penup()
+    pen.goto(0, 0)
+    pen.setheading(90)
+
+    for _ in range(12):
+        pen.fd(190)
+        pen.pendown()
+        pen.fd(20)
+        pen.penup()
+        pen.goto(0, 0)
+        pen.rt(30)
+
+    #hours
+    pen.penup()
+    pen.goto(0, 0)
+    pen.color("white")
+    pen.setheading(90)
+    angle = (h / 12) * 360
+    pen.rt(angle)
+    pen.pendown()
+    pen.fd(100)
+
+
+    #minutes
+    pen.penup()
+    pen.goto(0, 0)
+    pen.color("blue")
+    pen.setheading(90)
+    angle = (m / 60) * 360
+    pen.rt(angle)
+    pen.pendown()
+    pen.fd(180)
+
+    #seconds
+    pen.penup()
+    pen.goto(0, 0)
+    pen.color("red")
+    pen.setheading(90)
+    angle = (s / 60) * 360
+    pen.rt(angle)
+    pen.pendown()
+    pen.fd(140)
+
+while True:
+    h = int(time.strftime("%I"))
+    m = int(time.strftime("%M"))
+    s = int(time.strftime("%S"))
+
+    draw_clock(h, m, s, pen)
+   
+    wn.update()
+
+    time.sleep(1)
+    pen.clear()
+
+wn.mainloop()
+
+
+
